b3a9bc54d06af96e8f97e501baa6c9e810166ff4
Vladimir <vovchik.niko@gmail.com>
2022-07-18 18:53:24 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+  str = String(number);
+  if (str.length == 1){
+	switch(number){
+		case 0:
+			return zero;
+			break;
+		case 1:
+			return one;
+			break;
+		case 2:
+			return two;
+			break;
+		case 3:
+			return three;
+			break;
+		case 4:
+			return four;
+			break;
+		case 5:
+			return five;
+			break;
+		case 0:
+			return zero;
+			break;
+		case 0:
+			return zero;
+			break;
+	}
+  }
 }
